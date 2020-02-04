b4ac4baaa3b286be8aca4aeb2d0d9f4454119fd0
musicbender <musicbender@gmail.com>
2020-02-03 23:46:38 -0800

    models and queries for more health types

--- src/models/health/index.ts
+++ src/models/health/index.ts
@@ -1,6 +1,14 @@
 const healthTypes = require('../../configs/health.json');
 import Weight from './weight.model';
+import BodyTemperature from './body-temperature.model';
+import OxygenSaturation from './oxygen-saturation.model';
+import Height from './height.model'
+import WaistCircumference from './waist-circumference.model'
 
 export default {
-  [healthTypes.weight.modelID]: Weight
+  [healthTypes.weight.modelID]: Weight,
+  [healthTypes.bodyTemperature.modelID]: BodyTemperature,
+  [healthTypes.oxygenSaturation.modelID]: OxygenSaturation,
+  [healthTypes.height.modelID]: Height,
+  [healthTypes.waistCircumference.modelID]: WaistCircumference,
 }
