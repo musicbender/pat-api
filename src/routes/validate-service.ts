7bca25bbe81e319af50ab0962614d26192ea9424
musicbender <musicbender@gmail.com>
2021-03-30 22:07:04 -0700

    add versioning and version check endpoint

--- src/routes/validate-service.ts
+++ src/routes/validate-service.ts
@@ -1,23 +1,28 @@
 import * as Router from 'koa-router';
 import { restResponse } from '../utils/util';
 import { KoaContext } from 'types';
+const pkg = require('../../package.json');
 
 const router = new Router();
 
-router.get('/test', async (ctx: KoaContext) => {
+router.get('/test', async (ctx: KoaContext): Promise<void> => {
+  ctx.redirect('/health/liveliness');
+});
+
+router.get('/health/liveliness', async (ctx: KoaContext): Promise<void> => {
   restResponse(ctx);
 });
 
-router.get('/health/liveliness', async (ctx: KoaContext) => {
-  restResponse(ctx);
-});
-
-router.get('/health/readiness', async (ctx: KoaContext) => {
+router.get('/health/readiness', async (ctx: KoaContext): Promise<void>  => {
   if (ctx.isReady) {
     restResponse(ctx);
   } else {
-    restResponse(ctx, 500, 'not ready');
+    restResponse(ctx, 503, 'not ready');
   }
+});
+
+router.get('/version', async (ctx: KoaContext): Promise<void> => {
+  restResponse(ctx, 200, pkg.version);
 });
 
 export default router;
